66331d453ffe23ad502cdab2afeec8cef9cadf0a
Rodney Cobb <rcobb@theatlantic.com>
2018-04-27 12:26:26 -0400

    Added Security Groups - ensure you instantiate variables

--- main.tf
+++ main.tf
@@ -224,7 +224,7 @@ resource "aws_security_group" "wp_dev_sg" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["var.localip"]
+    cidr_blocks = ["${var.localip}"]
   }
 
   #HTTP
@@ -233,7 +233,7 @@ resource "aws_security_group" "wp_dev_sg" {
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cidr_blocks = ["var.localip"]
+    cidr_blocks = ["${var.localip}"]
   }
 
   egress {
