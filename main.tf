d6f47d0be68ef3be69ff2317e8afa10bca19d49f
Rodney Cobb <rcobb@theatlantic.com>
2018-04-30 08:52:30 -0400

    Added S3 bucket

--- main.tf
+++ main.tf
@@ -343,7 +343,7 @@ resource "random_id" "wp_code_bucket" {
   byte_length = 2
 }
 
-resource "aws_s3_bucket" {
+resource "aws_s3_bucket" "code" {
   bucket = "${var.domain_name}-${random_id.wp_code_bucket.dec}"
   acl = "private"
   force_destroy = true
