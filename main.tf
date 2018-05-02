2995cd00c90963d4fab402d1b99d6535acfbdcca
Rodney Cobb <rcobb@theatlantic.com>
2018-05-02 07:43:32 -0400

    Added RDS

--- main.tf
+++ main.tf
@@ -363,7 +363,7 @@ resource "aws_db_instance" "wp_db" {
   name = "${var.dbname}"
   username = "${var.dbuser}"
   password = "${var.dbpassword}"
-  aws_db_subnet_group_name = "${aws_db_subnet_group.wp_rds_subnetgroup.name}"
+  db_subnet_group_name = "${aws_db_subnet_group.wp_rds_subnetgroup.name}"
   vpc_security_group_ids = ["${aws_security_group.wp_rds_sg.id}"]
   skip_final_snapshot = true
 }
