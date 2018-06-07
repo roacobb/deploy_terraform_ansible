5e036141677b6bab8c5810f36a2490aaaf71254a
Rodney Cobb <rcobb@theatlantic.com>
2018-06-07 18:48:14 -0400

    Added Golden AMI

--- main.tf
+++ main.tf
@@ -441,6 +441,77 @@ resource "aws_elb" "wp_elb" {
   }
 }
 
+#------------- Golden AMI -----------
+
+
+# random ami id
+
+resource "random_id" "golden_ami" {
+  byte_length = 3
+}
+
+# AMI
+
+resource "aws_ami_from_instance" "wp_golden" {
+  name = "wp_ami-${random_id.golden_ami.b64}"
+  source_instance_id = "${aws_instance.wp_dev.id}"
+
+  provisioner "local-exec" {
+    command = << EOF
+cat <<EOF > userdata
+#!/bin/bash
+/usr/bin/aws s3 sync s3://${aws_s3_bucket.code.bucket} /var/www/html/
+/bin/touch /var/spool/cron/root 
+sudo /bin/echo '*/5 * * * * aws s3 sync s3://${aws_s3_bucket.code.bucket} /var/www/html' >> /var/spool/cron/root
+EOF
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
